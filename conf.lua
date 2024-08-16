233ba5fe0dc51a43e53fef01c46ed115c02748de
MatrixCode <jayden4er@posteo.de>
2024-08-16 13:11:15 +0200

    I AM A LINUX GOD (Or at least I managed to get everything to work on the linux computer)

--- conf.lua
+++ conf.lua
@@ -4,8 +4,7 @@ tile_size = 16
 function love.conf(t)
 	t.window.width = 320 * pixel_scale
 	t.window.height = 240 * pixel_scale
-	t.window.title = "Refreshing my brain for love2d"
+	t.window.title = "Hi eden, I did it! It works on the other laptop! BTW: How da heck did you manage to make all of this code so fast?"
 	t.modules.joystick = false
-	t.window.title = "Bonjure"
 end
 
