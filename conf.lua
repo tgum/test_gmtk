ed59a4c87ceda5d3031b1a61f78235c7a8180e4f
tegumai <tgum@protonmail.com>
2024-08-16 17:45:10 +0200

    changed the name of the window and added joke

--- conf.lua
+++ conf.lua
@@ -4,8 +4,8 @@ tile_size = 16
 function love.conf(t)
 	t.window.width = 320 * pixel_scale
 	t.window.height = 240 * pixel_scale
-	t.window.title = "Refreshing my brain for love2d"
+	t.window.title = "Preperation for GMTK 2024"
+	-- some ppl are into paris 2024, i think gmtk 2024 is more interesting
 	t.modules.joystick = false
-	t.window.title = "Bonjure"
 end
 
